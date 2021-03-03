dfde1d54aa4b26d7610de6f3ff7827591038a92f
ReCore <66930138+ReCore-sys@users.noreply.github.com>
2021-03-03 17:38:57 +1030

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,6 @@
+echo "HERE WE GOOOOOOOOOOOO"
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt get pip python-is-python3 gdebi openjdk-13-jre-headless proftpd realvnc-vnc-server openssh-server -y
+python3 -m pip install discord.py asyncio async_cse googlesearch-python better-profanity translate simpleeval
+echo "And we are done"
