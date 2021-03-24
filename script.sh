d753d8d31fcf816961bc3134dedb15e2aa942bbd
ReCore <66930138+ReCore-sys@users.noreply.github.com>
2021-03-25 09:31:32 +1030

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 echo "HERE WE GOOOOOOOOOOOO"
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install pip python-is-python3 gdebi openjdk-13-jre-headless proftpd realvnc-vnc-server openssh-server -y
-python3 -m pip install discord.py asyncio async_cse googlesearch-python better-profanity translate simpleeval cleverbotfree tinydb
+sudo apt-get install pip python-is-python3 gdebi default-jdk proftpd realvnc-vnc-server openssh-server -y
+python3 -m pip install discord.py asyncio async_cse googlesearch-python better-profanity translate simpleeval cleverbotfree tinydb wolframalpha faster-than-requests
 python3 -m pip install -U git+https://github.com/Rapptz/discord-ext-menus
 echo "And we are done"
